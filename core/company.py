82921bb1d9b6d020bbfaa7a3526a6796f7760574
Alexander Earl Harman <aeharman@purdue.edu>
2025-04-12 16:32:34 -0400

    Classify output

--- core/company.py
+++ core/company.py
@@ -1,8 +1,10 @@
 import click
-from core.utils import import_csv
+from core.utils import import_csv, split_department
 from core.client import Client
 from core.employee import Employee
 from data.db import insert_client, get_client_by_name, get_employees
+
+from risk_model.deployment import predict
 
 @click.command()
 @click.option('-n', '--name', type=str, required=True)
@@ -23,10 +25,18 @@ def fetch_results(client: str, cid: int):
 @click.command()
 @click.option('-c', '--client', type=str, required=True)
 def classify(client: str) -> dict:
-  data = get_employees
-  output = []
-  for row in data:
-    output.append(predict(row[0], row[1])) 
+  if client != None: 
+    data = get_employees(client)
+    output = []
+    for row in data:
+      # Name, Lit Score, Seniority, Degree Type, Gender, Department_HR, Department_Engineering, Age
+      # TODO Check this out to see if it works
+      t = split_department(row[5])
+
+      department_HR = t[0]
+      department_IT = t[1]
+
+      output.append(predict(row[0], row[1], row[2], row[3], row[4], department_HR, department_IT, row[6])) 
 
 @click.command()
 @click.option('-c', '--client', type=str)
