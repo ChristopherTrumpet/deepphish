b6f7be36028e176a7da13e199e39fa1d42cdc392
ChristopherTrumpet <christopher.m.t.2005@gmail.com>
2025-04-12 12:29:00 -0400

    more skeleton

--- cli.py
+++ cli.py
@@ -1,14 +1,28 @@
 import click
 from core.campaign import launch
-from core.templates import create_template
+from core.templates import create_template, list_templates, delete_template, view_template
+from core.company import add_client, classify, remove_client, list_clients
 
 @click.group()
 def cli():
   """A cli interface for the deep phish security app"""
   pass
 
+# Campaigning
 cli.add_command(launch)
+
+# Templates
 cli.add_command(create_template)
+cli.add_command(delete_template)
+cli.add_command(view_template)
+cli.add_command(list_templates)
+
+# Company commands
+cli.add_command(add_client)
+cli.add_command(remove_client)
+cli.add_command(classify)
+cli.add_command(list_clients)
+
 
 if __name__ == "__main__":
   cli()
--- core/templates.py
+++ core/templates.py
@@ -1,7 +1,22 @@
 import click
 
 @click.command()
-@click.option('t', '--name', type=str, required=True, help="Name of the template for identification")
-@click.option('-d', '--difficulty', type=str, required=True, help="Difficulty of template")
-def create_template():
+@click.option('-n', '--name', type=str, required=True, help="Name of the template for identification")
+@click.option('-d', '--difficulty', type=click.Choice(['easy', 'medium', 'hard'], case_sensitive=False), required=True, help="Difficulty of template")
+def create_template(name: str, difficulty: str):
+  pass
+
+@click.command()
+@click.option('-n', '--name', type=str, required=True)
+def view_template(name: str):
+  pass
+
+@click.command()
+@click.option('-cid', '--company-id', type=int, required=False)
+def list_templates(c_id):
+  pass
+
+@click.command()
+@click.option('-n', '--name', type=str, required=True)
+def delete_template(name):
   pass
