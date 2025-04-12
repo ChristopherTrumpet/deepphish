acf8817c00fa1dd81cf8da49e22a5a460db287b8
Augusto Butkewitsch <augustobutkewitsch@gmail.com>
2025-04-12 19:47:01 -0400

    link tracker

---
+++ llm/link.py
@@ -0,0 +1,4 @@
+script_url = 'https://script.google.com/macros/s/AKfycbwUeN-wsnMWy8D8ngxwWjBcRUng5Bc1SmGZP4whkZ0XIzSXMMOU6O4OreLXFTobvxOE2A/exec'
+
+def get_unique_url(user):
+    return f'{script_url}?user={user}'
